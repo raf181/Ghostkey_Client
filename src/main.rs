8625e2cc762b6bf8effa1282035898273e2f8f99
raf181 <93051606+raf181@users.noreply.github.com>
2024-07-06 20:18:37 +0200

    chore: Update subcommand handling and server addresses

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,24 @@ fn main() -> Result<(), Box<dyn Error>> {
     let matches = build_cli().get_matches();
 
     match matches.subcommand() {
-        Some(("register_user", sub_m)) => register_user(sub_m)?,
-        Some(("login", sub_m)) => login(sub_m)?,
-        Some(("send_command", sub_m)) => send_command(sub_m)?,
-        _ => unreachable!(),
+        Some(("register_user", sub_m)) => {
+            println!("Registering user...");
+            register_user(sub_m)?
+        }
+        Some(("login", sub_m)) => {
+            println!("Logging in...");
+            login(sub_m)?
+        }
+        Some(("send_command", sub_m)) => {
+            println!("Sending command...");
+            send_command(sub_m)?
+        }
+        Some((name, _)) => {
+            println!("Unmatched subcommand: {}", name);
+        }
+        None => {
+            println!("No subcommand matched.");
+        }
     }
 
     Ok(())
@@ -24,7 +38,7 @@ fn register_user(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
     let password = matches.get_one::<String>("password").unwrap();
 
     let client = Client::new();
-    let res = client.post("http://your_server_address/register_user")
+    let res = client.post("http://192.168.1.62:5000/register_user")
         .form(&[("secret_key", secret_key), ("username", username), ("password", password)])
         .send()?;
 
@@ -37,7 +51,7 @@ fn login(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
     let password = matches.get_one::<String>("password").unwrap();
 
     let client = Client::new();
-    let res = client.post("http://your_server_address/login")
+    let res = client.post("http://192.168.1.62:5000/login")
         .form(&[("username", username), ("password", password)])
         .send()?;
 
@@ -50,7 +64,7 @@ fn send_command(matches: &ArgMatches) -> Result<(), Box<dyn Error>> {
     let command = matches.get_one::<String>("command").unwrap();
 
     let client = Client::new();
-    let res = client.post("http://your_server_address/command")
+    let res = client.post("http://192.168.1.62:5000/command")
         .form(&[("esp_id", esp_id), ("command", command)])
         .send()?;
 
